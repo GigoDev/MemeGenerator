26c947cc9aaecd4739e8cf015de2de29d2bfc71b
Gigo Dev <guyrick21@gmail.com>
2024-06-14 13:31:15 +0300

    Add user text input

--- js/memeController.js
+++ js/memeController.js
@@ -4,33 +4,43 @@ var gCtx
 function onInit() {
     gElCanvas = document.querySelector('canvas')
     gCtx = gElCanvas.getContext('2d')
+
+    resizeCanvas()
+    window.addEventListener('resize', resizeCanvas)
     renderMeme()
 }
 
 function renderMeme() {
-    let meme = getMeme()
-    drawImg(meme.selectedImgId)
-    drawText('hello', 100, 100)
+    const meme = getMeme()
+    const top = { x: gElCanvas.width/2, y: 20 }
+    const bottom = { x: gElCanvas.width/2, y: gElCanvas.height - 20 }
+
+    drawImg(meme.selectedImgId, () => drawText(gMeme.lines[0].txt, gMeme.lines[0].size, top))
 }
 
-function drawText(text, x, y) {
-    gCtx.lineWidth = 2
-    gCtx.strokeStyle = 'brown'
-    gCtx.fillStyle = 'black'
-    gCtx.font = '40px Arial'
+function drawImg(id, callback) {
+    const elImg = new Image()
+    elImg.src = `imgs/${id}.jpg`
+    elImg.onload = () => {
+        gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+        callback()
+    }
+}
+
+function drawText(text, size, {x, y}) {
+    console.log(x)
+    console.log(y)
+    
+    gCtx.lineWidth = size / 20
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = 'white'
+    gCtx.font = `${size}px Arial`
     gCtx.textAlign = 'center'
     gCtx.textBaseline = 'middle'
     gCtx.fillText(text, x, y)
     gCtx.strokeText(text, x, y)
 }
 
-function drawImg(id) {
-    const elImg = new Image()
-    elImg.src = `imgs/${id}.jpg`
-    elImg.onload = () => {
-        gCtx.drawImage(elImg, 0, 0, elImg.naturalWidth, elImg.naturalHeight)
-    }
-}
 
 function onSelectImg(elImg) {
     coverCanvasWithImg(elImg)
@@ -43,12 +53,19 @@ function coverCanvasWithImg(elImg) {
 
 function resizeCanvas() {
     const elContainer = document.querySelector('.canvas-container')
-    gElCanvas.width = elContainer.clientWidth - 2
-    renderImg()
+    gElCanvas.width = elContainer.offsetWidth
+    gElCanvas.height = elContainer.offsetHeight
+    renderMeme()
+
 }
 
 function renderImg(src = 'img/square.jpg') {
     const elImg = new Image()
     elImg.src = src
     coverCanvasWithImg(elImg)
+}
+
+function onGetUserText(text) {
+    setLineTxt(text)
+    renderMeme()
 }
--- js/memeService.js
+++ js/memeService.js
@@ -1,17 +1,21 @@
-let gImgs = [{id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat']}]
+let gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
 
 let gMeme = {
     selectedImgId: 1,
     selectedLineIdx: 0,
     lines: [
-    {
-    txt: 'I sometimes eat Falafel',
-    size: 20,
-    color: 'red'
-    }
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            color: 'red'
+        }
     ]
-   }
-   
-   function getMeme(){
+}
+
+function getMeme() {
     return gMeme
-   }
+}
+
+function setLineTxt(txt) {
+    gMeme.lines[0].txt = txt
+}
